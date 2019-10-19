18363d8094e34f687dd51184e998505841e1dbae
PatrykIgras <igras.patryk@gmail.com>
2019-10-19 16:55:08 +0200

    add movie service impl

--- src/main/java/com/example/cinema/repository/MovieRepository.java
+++ src/main/java/com/example/cinema/repository/MovieRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface MovieRepository extends PagingAndSortingRepository<Movie, Long> {
     Page<Movie> findByCategory(EMovieCategory category, Pageable pageable);
-    Page<Movie> finfByTitleContaining(String partOfTitle, Pageable pageable);
+    Page<Movie> findByTitleContaining(String partOfTitle, Pageable pageable);
 }
