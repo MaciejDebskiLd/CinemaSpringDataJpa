6017cdba10732879a44b6552633e412b00af8540
PatrykIgras <igras.patryk@gmail.com>
2019-10-20 09:15:33 +0200

    some reformat

--- src/main/java/com/example/cinema/service/MovieServiceImpl.java
+++ src/main/java/com/example/cinema/service/MovieServiceImpl.java
@@ -20,19 +20,20 @@ public class MovieServiceImpl implements MovieService {
     private MovieRepository movieRepository;
     private PosterRepository posterRepository;
 
-    public MovieServiceImpl (MovieRepository movieRepository, PosterRepository posterRepository) {
-            this.movieRepository = movieRepository;
-            this.posterRepository = posterRepository;
-}
+    public MovieServiceImpl(MovieRepository movieRepository, PosterRepository posterRepository) {
+        this.movieRepository = movieRepository;
+        this.posterRepository = posterRepository;
+    }
+
     @Override
     @Transactional
-    public Long createMovie (String title, EMovieCategory category, Integer length, String description, Integer requiredAge, String posterFilePath) {
-    Movie movie = new Movie(null, title, category, length, description, requiredAge);
+    public Long createMovie(String title, EMovieCategory category, Integer length, String description, Integer requiredAge, String posterFilePath) {
+        Movie movie = new Movie(null, title, category, length, description, requiredAge);
 
-    movieRepository.save (movie);
-    if (posterFilePath!=null) {
-    createPoster (movie, posterFilePath) ;
-    }
+        movieRepository.save(movie);
+        if (posterFilePath != null) {
+            createPoster(movie, posterFilePath);
+        }
         return movie.getId();
     }
 
@@ -41,23 +42,30 @@ public class MovieServiceImpl implements MovieService {
         poster.setMovie(movie);
         posterRepository.save(poster);
     }
-    @Override
-    public Optional<Movie> getMovie (Long movieId) { return movieRepository.findById(movieId); }
 
     @Override
-    public Page<Movie> getMoviesInCategory (EMovieCategory category, Pageable pageable) {
+    public Optional<Movie> getMovie(Long movieId) {
+        return movieRepository.findById(movieId);
+    }
+
+    @Override
+    public Page<Movie> getMoviesInCategory(EMovieCategory category, Pageable pageable) {
         return movieRepository.findByCategory(category, pageable);
     }
 
     @Override
-    public Page<Movie> getMoviesByPartOfTitle (String partOfTitle, Pageable pageable) {
+    public Page<Movie> getMoviesByPartOfTitle(String partOfTitle, Pageable pageable) {
         return movieRepository.findByTitleContaining(partOfTitle, pageable);
     }
+
     @Override
-    public Page<Movie> getAllMovies(Pageable pageable) { return movieRepository.findAll(pageable); }
+    public Page<Movie> getAllMovies(Pageable pageable) {
+        return movieRepository.findAll(pageable);
+    }
+
     @Override
     @Transactional
-    public void updateMovie (Long movieId, String title, EMovieCategory category, Integer length, String description, Integer requiredAge, String posterFilePath) {
+    public void updateMovie(Long movieId, String title, EMovieCategory category, Integer length, String description, Integer requiredAge, String posterFilePath) {
         Optional<Movie> movieOptional = movieRepository.findById(movieId);
         if (!movieOptional.isPresent()) {
             throw new EntityDoesNotExistException("Movie, id=" + movieId);
@@ -85,7 +93,10 @@ public class MovieServiceImpl implements MovieService {
             createPoster(movie, posterFilePath);
         }
     }
+
     @Override
     @Transactional
-    public void removeMovie (Long movieId) { movieRepository.deleteById(movieId);}
+    public void removeMovie(Long movieId) {
+        movieRepository.deleteById(movieId);
+    }
 }
